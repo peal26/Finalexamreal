4616be07b631af29d2c2f679a84fdf6840545989
Phanhiran <peal_26@hotmail.com>
2014-12-02 14:16:47 +0700

    ff

---
+++ lib/Finalexam.rb
@@ -0,0 +1,24 @@
+class Finalexam
+	
+	l = 1
+	def lovemac(numbers)
+		l = 1
+		@numberkub=numbers
+		while l < numbers
+		
+		if l%15 == 0
+		puts "HateWindows"
+		
+		elsif l%5 == 0
+		puts "mac"
+	elsif l%3 == 0
+        puts "love"
+	end
+	l = l + 1
+		
+	end
+	
+	
+
+
+end
---
+++ spec/lib/Finalexam_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+require "Finalexam"
+
+describe Finalexam do
+  describe "#run the program" do
+    it "run the program ra na kub" do
+   	  (1..50).each do |number|
+        puts lovemac(number)
+    end
+    
+   
+end
+end
